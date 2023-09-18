931aa603df826dd2e27f63d1bcb6fab68e99e12f
jjgodcom <jjgodcom1@gmail.com>
2023-09-18 20:43:45 +0900

    🥳 컴퍼넌트완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import HomeView from "./views/HomeView";
+import AboutView from "./views/AboutView";
 
 const App = () => {
-    return <div>Appaaaaa</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomeView />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
