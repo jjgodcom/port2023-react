c8a9083919f25fb156c764a96a0e375c3c8f3234
jjgodcom <jjgodcom1@gmail.com>
2023-09-19 02:39:26 +0900

    😱 푸터 완료

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <footer id="footer" role="contentinfo">
+            <div className="footer__inner">
+                <h2 className="footer__text">
+                    <div>webstoryboy</div>
+                    <div>@webs</div>
+                </h2>
+                <div className="footer__right">
+                    &copy; 2023 webstoryboy<br />
+                    이 사이트는 리액트를 이용하여 제작하였습니다.
+                </div>
+            </div>
+        </footer>
+    )
 }
 
-export default Footer
+export default Footer;
