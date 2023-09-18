bd979a5dca2988fcfb1de77426d576e924f4b491
jjgodcom <jjgodcom1@gmail.com>
2023-09-19 02:55:08 +0900

    😱 마무리 작업

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import HomeView from "./views/HomeView";
-import AboutView from "./views/AboutView";
+import lenis from "./utils/lenis";
+import link from "./utils/link";
 
 const App = () => {
+    useEffect(() => {
+        lenis();
+        link();
+    }, []);
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomeView />} />
-            </Routes>
-        </BrowserRouter>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomeView />} />
+        </Routes>
+    </BrowserRouter>
     );
 };
 
