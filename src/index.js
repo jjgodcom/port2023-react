d984f9456fb7ade7605de93c9d48465cf175415e
jjgodcom <jjgodcom1@gmail.com>
2023-09-19 01:28:59 +0900

    😱 인트로 완료

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./assets/scss/style.scss";
+import "./assets/scss/style.scss"; // 여기에 선언하면 전역에서 스타일 사용 가능
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
